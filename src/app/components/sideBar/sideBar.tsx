3ff9af19f7d553c94ca1152cb921c61fe72e155e
rafael Pietri-Davila <rafael.pietri@gmail.com>
2023-12-01 18:16:10 +0100

    optimized code for sidbar component

--- src/app/components/sideBar/sideBar.tsx
+++ src/app/components/sideBar/sideBar.tsx
@@ -4,32 +4,41 @@ import { useAppSelector } from "@/app/redux/hooks";
 import SelectSearchOptions from "../searchForm/selectSearchOptions";
 import StudentForm from "../forms/studentForm";
 import InstrumentForm from "../forms/instrumentForm";
-import React from "react";
 
 export default function SideBar() {
   const selectOption = useAppSelector((state) => state.searchOptions.type);
 
-  return (
-    <aside className="flex flex-col basis-1/4 items-center bg-slate-700 mt-2 ml-2 rounded-lg">
-      <SelectSearchOptions>
-        {selectOption === "Search Student" && (
+  function getFormComponent(selectOption: string) {
+    switch (selectOption) {
+      case "Search Student":
+        return (
           <StudentForm formTitle="Search Student" buttonText="Search Student" />
-        )}
-        {selectOption === "Search Instrument" && (
+        );
+
+      case "Search Instrument":
+        return (
           <InstrumentForm
             formTitle="Search Instrument"
             buttonText="Search Instrument"
           />
-        )}
-        {selectOption === "Add Student" && (
-          <StudentForm formTitle="Add Student" buttonText="Add Student" />
-        )}
-        {selectOption === "Add Instrument" && (
+        );
+
+      case "Add Student":
+        return <StudentForm formTitle="Add Student" buttonText="Add Student" />;
+
+      case "Add Instrument":
+        return (
           <InstrumentForm
             formTitle="Add Instrument"
             buttonText="Add Instrument"
           />
-        )}
+        );
+    }
+  }
+  return (
+    <aside className="flex flex-col basis-1/4 items-center bg-slate-700 mt-2 ml-2 rounded-lg">
+      <SelectSearchOptions>
+        {getFormComponent(selectOption)}
       </SelectSearchOptions>
     </aside>
   );
