0e4da27964a8ec7d87bccc9c4c0b3b0be9eb01a8
rafael Pietri-Davila <rafael.pietri@gmail.com>
2024-01-30 15:01:01 +0100

    added ui designs in studentCard.tsx

--- src/app/components/cards/studentCard.tsx
+++ src/app/components/cards/studentCard.tsx
@@ -8,16 +8,18 @@ export default function StudentCard({ student }: StudentCardProps) {
   return (
     <>
       <div className="flex flex-row justify-evenly bg-white w-3/4 rounded-lg m-4 basis-1/3">
-        <div className="flex flex-col w-1/3 items-center justify-center">
-          <h1 className="text-2xl font-bold">
-            {student.firstName} {student.lastName}
-          </h1>
-          <p>
-            <strong>Student Id : </strong>
-            {student.studentIdNumber}
-          </p>
+        <div className="flex flex-row w-full items-center justify-evenly">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold">
+              {student.firstName} {student.lastName}
+            </h1>
+            <p>
+              <strong>Student Id : </strong>
+              {student.studentIdNumber}
+            </p>
+          </div>
           {student.instrument ? (
-            <div>
+            <div className="flex flex-col">
               <p>
                 <strong>Type: </strong>
                 {student.instrument?.type}
