8d5b7564bab24d8aca0d9b47665d28e4b6b962aa
rafael Pietri-Davila <rafael.pietri@gmail.com>
2024-02-29 15:51:26 +0100

    added color options when user is logged in or not

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -2,20 +2,31 @@ type ButtonProps = {
   name: string;
   marginTop: string;
   type: "submit" | "reset" | "button";
-  onClick: () => void;
+  disabled?: boolean;
+  onClick?: () => void;
+  disabledColor?: boolean;
 };
 
 export default function Button({
   name,
   type,
   marginTop,
+  disabled,
   onClick,
+  disabledColor,
 }: ButtonProps): React.JSX.Element {
   const buttonClass = `bg-blue-500 hover:bg-blue-700 text-white p-2 mt-${marginTop} w-full rounded-lg`;
-
+  const greyColor = `bg-gray-300 text-black p-2 mt-${marginTop} w-full rounded-lg`;
   return (
-    <button className={buttonClass} type={type} onClick={onClick}>
-      {name}
-    </button>
+    <>
+      <button
+        className={disabledColor ? greyColor : buttonClass}
+        type={type}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {name}
+      </button>
+    </>
   );
 }
