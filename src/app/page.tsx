64e79f26504a5711ef4b5f1292705d58e21cc727
rafael Pietri-Davila <rafael.pietri@gmail.com>
2024-03-14 17:11:44 +0100

    added userDetail component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import UserDetail from "./components/userDetail/userDetail";
+import { useAppSelector } from "./redux/hooks";
 export default function Home() {
+  const isLoggedIn = useAppSelector((state) => state.userInfo.isLoggedIn);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between basis-3/4 bg-white mt-2 rounded-lg">
       <div className="max-w-5xl w-full items-center justify-center font-mono lg:flex">
-        <p>Home</p>
+        {isLoggedIn && <UserDetail />}
       </div>
     </main>
   );
