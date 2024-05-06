9985e2595b5df2b3519dbe0cd7dccdfa692e768e
rpdavila <rafael.pietri@gmail.com>
2024-05-06 16:04:39 +0200

    change back tpo string then env variables

--- src/app/utilities/mongodb.ts
+++ src/app/utilities/mongodb.ts
@@ -12,12 +12,10 @@ export const app = new Realm.App(config)
 //export const app = Realm.getApp(process.env.NEXT_PUBLIC_APP_ID as string)
 const client = app.currentUser?.mongoClient("mongodb-atlas")
 
-const db = client?.db(process.env.NEXT_PUBLIC_DB_NAME as string); 
-export const studentCollection =  db?.collection(process.env.NEXT_PUBLIC_DB_STUDENT_COLLECTION as string);
-export const instrumentCollection =  db?.collection(process.env.NEXT_PUBLIC_DB_INSTRUMENT_COLLECTION as string)
-export const userCollection = db?.collection(
-  process.env.NEXT_PUBLIC_DB_USER_COLLECTION as string
-);
+const db = client?.db("Test"); 
+export const studentCollection =  db?.collection("studentInfo");
+export const instrumentCollection =  db?.collection("instrumentInfo")
+export const userCollection = db?.collection("users");
 export function convertObjectIdToString(result: any[] | undefined) {
   result?.forEach((document) => {
     document._id = String(document._id);
