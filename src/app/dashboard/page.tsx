01be335c943a67aba84374bdcb5c0602ec682150
rpdavila <rafael.pietri@gmail.com>
2024-05-16 17:54:48 +0200

    added check for persisted data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+"use strinct";
 import { useEffect } from "react";
 import UserDetail from "../components/userDetail/userDetail";
 import { useAppSelector, useAppDispatch } from "../lib/ReduxSSR/hooks";
@@ -8,22 +9,19 @@ import { getCustomUserData } from "../lib/ReduxSSR/features/userSlice";
 
 export default function DashBoardMainPage() {
   const dispatch = useAppDispatch();
-  const instruments = useAppSelector((state) => state.instruments.instrumentList);
-  const students = useAppSelector((state) => state.students.studentList);
-  const dropDownList = useAppSelector((state) => state.students.dropDownList)
-  const userData = useAppSelector((state) => state.userInfo.customUserData)
-
 
   useEffect(() => {
-    instruments.length === 0 ? dispatch(getInstruments()) : null;
-    students.length === 0 ? dispatch(getStudents()) : null;
-    dropDownList.length === 0 ? dispatch(getDropDownList()) : null;
-    userData === undefined ? dispatch(getCustomUserData()) : null
-
-  }, [instruments.length, students.length, dropDownList.length, userData, dispatch])
-  return (
-    <section className="flex flex-col min-h-screen bg-white mt-2 rounded-lg basis-3/4 items-center">
-      <UserDetail />
-    </section>
-  );
+    const persistedData = window.localStorage.getItem('persist:root');
+    if (!persistedData) {
+      dispatch(getInstruments())
+      dispatch(getStudents())
+      dispatch(getDropDownList())
+      dispatch(getCustomUserData())
+    } 
+  }, [dispatch])
+return (
+  <section className="flex flex-col min-h-screen bg-white mt-2 rounded-lg basis-3/4 items-center">
+    <UserDetail />
+  </section>
+);
 }
