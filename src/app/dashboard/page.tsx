d6597dfa2ac1e0c64d6526cabcddf67942a803a6
rpdavila <rafael.pietri@gmail.com>
2024-05-16 17:18:37 +0200

    removed checks in dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,18 +6,6 @@ import { getStudents, getDropDownList } from "../lib/ReduxSSR/features/studentLi
 import { getCustomUserData } from "../lib/ReduxSSR/features/userSlice";
 
 export default function DashBoardMainPage() {
-  const dispatch = useAppDispatch();
-  const instruments = useAppSelector((state) => state.instruments.instrumentList);
-  const students = useAppSelector((state) => state.students.studentList);
-  const dropDownList = useAppSelector((state) => state.students.dropDownList)
-  const userData = useAppSelector((state) => state.userInfo.customUserData)
-  instruments.length === 0 ? dispatch(getInstruments()) : null;
-  students.length === 0 ? dispatch(getStudents()) : null;
-  dropDownList === undefined ? dispatch(getDropDownList()) : null;
-  if (userData === undefined) {
-    dispatch(getCustomUserData())
-  }
-
   return (
     <section className="flex flex-col min-h-screen bg-white mt-2 rounded-lg basis-3/4 items-center">
       <UserDetail />
