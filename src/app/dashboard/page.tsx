f76bb6525916ae508a6ee3b08479321db2ddcf45
rpdavila <rafael.pietri@gmail.com>
2024-05-17 09:57:50 +0200

    added laoding functioanlity to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { useRef, useEffect } from "react";
-import { useAppStore } from "@/app/lib/ReduxSSR/hooks";
+import { useAppSelector, useAppStore } from "@/app/lib/ReduxSSR/hooks";
 import { getDropDownList, getStudents } from "@/app/lib/ReduxSSR/features/studentListSlice";
 import { getInstruments } from "@/app/lib/ReduxSSR/features/instrumentSLice";
 
 import UserDetail from "../components/userDetail/userDetail";
 
-
-
 export default function DashboardMainPage() {
   const store = useAppStore()
   const initialized = useRef(false)
+  const studentsLoading = useAppSelector((state) => state.students.loading)
+  const instrumentsLoading = useAppSelector((state) => state.instruments.loading)
+  const dropDownListLoading = useAppSelector((state) => state.students.loading)
 
   useEffect(() => {
-    
     if (!initialized.current) {
       store.dispatch(getStudents())
       store.dispatch(getInstruments())
@@ -22,9 +22,14 @@ export default function DashboardMainPage() {
     }
   })
 
+
+  if (studentsLoading || instrumentsLoading || dropDownListLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <section className="flex flex-col min-h-screen bg-white mt-2 rounded-lg basis-3/4 items-center">
-      {initialized ? <UserDetail /> : "Initializing your data"}
+      <UserDetail />
     </section>
   );
 }
