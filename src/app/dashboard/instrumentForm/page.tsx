cc63dc202d27db058903b6584db4769284d4ff3a
rpdavila <rafael.pietri@gmail.com>
2024-11-07 10:48:11 +0100

    removed unused components and custom hooks

--- src/app/dashboard/instrumentForm/page.tsx
+++
@@ -1,7 +0,0 @@
-import InstrumentForm from "@/app/components/forms/instrumentForm";
-
-export default function AddInstrument() {
-  return (
-    <InstrumentForm formTitle="Add Instrument" />
-  )
-}
--- src/app/dashboard/studentForm/page.tsx
+++
@@ -1,7 +0,0 @@
-import StudentForm from "@/app/components/forms/studentForm";
-
-export default function AddStudent() {
-  return (    
-    <StudentForm formTitle="Add Student" />    
-  )
-}
