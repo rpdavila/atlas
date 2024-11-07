6b3ad792d294b1a3b9ed21d56ee7786782279cbd
rpdavila <rafael.pietri@gmail.com>
2024-11-07 11:02:08 +0100

    added keys to iterate for pull down menus

---
+++ src/app/data/roles.ts
@@ -0,0 +1,4 @@
+export const roles = [
+  { key: 'Teacher', label: 'Teacher' },
+  { key: 'Recruiter', label: 'Recruiter' }
+]
--- src/app/data/tools.ts
+++ src/app/data/tools.ts
@@ -1,6 +1,7 @@
 export const tools = [
-  {key: "Add Student", label: "Add Student"},
-  {key: "Add Instrument", label: "Add Instrument"}, 
-  {key: "Search Student", label: "Search Student"},
-  {key: "Search Instrument", label: "Search Instrument"},  
+  { key: "Add Student", label: "Add Student" },
+  { key: "Add Instrument", label: "Add Instrument" },
+  { key: "Search Student", label: "Search Student" },
+  { key: "Search Instrument", label: "Search Instrument" },
+  { key: "Search District", label: "Search District" }
 ];
