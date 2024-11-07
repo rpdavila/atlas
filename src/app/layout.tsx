3d5470154497573d5468e8f0fce250fc2acaaddc
rpdavila <rafael.pietri@gmail.com>
2024-11-07 09:59:47 +0100

    created a standard layout and removed all html into their proper components in the page.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./globals.css";
 import type { Metadata } from "next";
-import {useRouter} from "next/router";
+
 
 import { Inter } from "next/font/google";
 
 import { Providers } from "@/app/redux/provider";
-
-
 import Header from "./components/header/header";
+import { NextAuthProvider } from "./nextAuthProvider/nextAuthProvider";
+import { Link } from "@nextui-org/react";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Music Inventory Solutions",
-  description: "Manage your music classroom inventory more efficiently",
+  title: "CrescendoSync Systems",
+  description: "Innovative Music Inventory Management System",
 };
 
 export default function RootLayout({
@@ -25,14 +25,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>        
-        <Providers>
-          <Header />
-          <main className="min-h-screen bg-white flex justify-center">
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <NextAuthProvider>
+          <Providers>
+            <Header />
+            <main className="flex flex-col justify-center flex-1 px-4 sm:px-6 lg:px-8 text-center bg-slate-700">
+
             {children}
-          </main>
-        </Providers>
-        
+
+            <footer className="flex items-center justify-center w-full h-24 border-t mt-8">
+              <Link href="/" target="_blank" rel="noopener noreferrer">
+                Powered by Crescendo Cloud
+              </Link>
+            </footer>
+            </main>
+          </Providers>
+        </NextAuthProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import Services from "./components/servicesDescription/services";
+
 export default function Home() {
-  return (
-    <section className="flex min-h-screen flex-col items-ceter justify-between">
-      <div className="max-w-5xl items-center justify-center font-mono">
-        <p>Home</p>
-      </div>
-    </section>
+  return (    
+    <Services />
   );
 }
