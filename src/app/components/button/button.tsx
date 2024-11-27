8a09767c304e1c9094320f0fe2d7e942ac009e89
rpdavila <rafael.pietri@gmail.com>
2024-11-27 18:23:43 +0100

    added danger option in button component

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -9,6 +9,7 @@ type ButtonProps = {
   onClick?: () => void;
   disabledColor?: boolean;
   icon?: React.JSX.Element;
+  danger?: boolean
 };
 
 export default function Button({
@@ -16,20 +17,21 @@ export default function Button({
   type,
   marginTop,
   onClick,
-  icon
+  icon,
+  danger
 }: ButtonProps): React.JSX.Element {
   const buttonClass = `bg-blue-500 hover:bg-blue-700 text-white p-2 mt-${marginTop} w-full sm:w-full rounded-lg`;
   const greyColor = `bg-gray-300 text-black p-2 mt-${marginTop} w-full rounded-lg`;
-
+  const dangerColor = `bg-red-500 hover:bg-red-700 text-white p-2 mt-${marginTop} w-full sm:w-full rounded-lg`;
   const { pending } = useFormStatus();
   return (
     <button
-      className={pending ? greyColor : buttonClass}
+      className={pending ? greyColor : danger ? dangerColor : buttonClass}
       type={type}
       onClick={onClick}
       disabled={pending}
 
-    > 
+    >
       <div className="flex flex-row justify-center items-center gap-2">
         {pending && <Spinner size="sm" color="primary" />}
         {icon && icon}
