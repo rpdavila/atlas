a18c4c495f629b2caf81b2ac04aedc8903ffa0ae
rpdavila <rafael.pietri@gmail.com>
2025-01-02 14:09:17 +0100

    added optional chaingin in if statement on line 39-42

--- src/app/components/searchDistrictInstruments/searchDistrictInstruments.tsx
+++ src/app/components/searchDistrictInstruments/searchDistrictInstruments.tsx
@@ -1,35 +1,50 @@
 "use client";
 //types
-import { DistrictInstrumentsWithouUserId, DistrictList } from "@/app/types/formTypes"
+
 import { RootState } from "@/lib/ReduxSSR/store";
 //redux imports
 import { useAppSelector } from "@/lib/ReduxSSR/hooks"
 import InstrumentSearchForm from "../forms/instrumentSearchForm";
 import DistrictInstrumentCardList from "../card-list/districtInstrumentCardList";
+import { RentStatus } from "@prisma/client";
+type DistrictInstrument = {
+  school: {
+    name: string;
+  };
+  id: string;
+  classification: string;
+  brand: string;
+  serialNumber: string;
+  rentStatus: RentStatus;
+} | undefined
+
+type DistrictInstruments = Array<DistrictInstrument>
+
+
 export default function SearchDistrictInstruments(
   {
     displayInstruments
   }:
     {
-      displayInstruments: DistrictList
+      displayInstruments: DistrictInstruments
     }) {
 
   // grab search field from redux
   const searchField = useAppSelector((state: RootState) => state.searchOptions.search)
-  let instrumentSearchResults: DistrictList = []
+  let instrumentSearchResults: DistrictInstruments = []
   // get user search params
   if (!!displayInstruments) {
-    instrumentSearchResults = displayInstruments.filter((instrument: DistrictInstrumentsWithouUserId) => {
+    instrumentSearchResults = displayInstruments.filter((instrument: DistrictInstrument) => {
       return (
-        instrument.classification.includes(searchField) ||
-        instrument.brand?.includes(searchField) ||
-        instrument.serialNumber?.includes(searchField) ||
-        instrument.school?.name.includes(searchField)
+        instrument?.classification.includes(searchField) ||
+        instrument?.brand?.includes(searchField) ||
+        instrument?.serialNumber?.includes(searchField) ||
+        instrument?.school?.name.includes(searchField)
       )
     })
   }
   return (
-    <section className={`flex flex-col basis-3/4 w-full items-center p-2 gap-2`}>
+    <section className={`flex flex-col basis-3/4 w-full items-center p-2 gap-2 h-svh`}>
       <section className="w-full md:hidden">
         <InstrumentSearchForm />
       </section>
