373bb1fe31737f256027b12655d6f4142e485854
rpdavila <rafael.pietri@gmail.com>
2025-01-06 13:07:39 +0100

    feat: enhance StudentInfo component with error handling and type definitions

--- src/app/dashboard/studentInfo/[id]/page.tsx
+++ src/app/dashboard/studentInfo/[id]/page.tsx
@@ -1,17 +1,41 @@
 import { getStudentById } from "@/actions/actions"
-export default async function StudentInfo(
-  { params }:
-    {
-      params: { id: string }
-    }) {
-  const student = await getStudentById(params.id)
-  return (
-    <section className="bg-slate-100 rounded-lg">
-      <h1 className="text-2xl">
-        Name: {student?.firstName} {student?.lastName}
-      </h1>
-      <p>Student ID Number: {student?.studentIdNumber}</p>
-      <p>School: {student?.school?.name}</p>
-    </section>
-  )
+import { notFound } from "next/navigation"
+type Props = {
+  params: { id: string }
+}
+type Student = {
+  firstName: string;
+  lastName: string;
+  studentIdNumber: string;
+  instrumentAssignment: {
+    instrument: {
+      classification: string;
+      brand: string;
+      serialNumber: string;
+    };
+  } | null;
+  school?: {
+    name: string
+  } | null;
+} | null
+
+export default async function StudentInfo({ params }: Props) {
+  try {
+    const student: Student = await getStudentById(params.id)
+    if (!student) {
+      notFound()
+    }
+
+    return (
+      <section className="bg-slate-100 rounded-lg">
+        <h1 className="text-2xl">
+          Name: {student?.firstName} {student?.lastName}
+        </h1>
+        <p>Student ID Number: {student?.studentIdNumber}</p>
+        <p>School: {student?.school?.name}</p>
+      </section>
+    )
+  } catch (error) {
+    throw error
+  }
 }
