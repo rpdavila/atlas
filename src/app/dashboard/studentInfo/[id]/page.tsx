22d13c7cf99c1eac48639238e30b4d5d2bfc8719
rpdavila <rafael.pietri@gmail.com>
2025-01-06 13:21:19 +0100

    refactor: remove unnecessary type definition and simplify student data access in StudentInfo component

--- src/app/dashboard/studentInfo/[id]/page.tsx
+++ src/app/dashboard/studentInfo/[id]/page.tsx
@@ -1,25 +1,10 @@
 import { getStudentById } from "@/actions/actions"
 import { notFound } from "next/navigation"
 
-type Student = {
-  firstName: string;
-  lastName: string;
-  studentIdNumber: string;
-  instrumentAssignment: {
-    instrument: {
-      classification: string;
-      brand: string;
-      serialNumber: string;
-    };
-  } | null;
-  school?: {
-    name: string
-  } | null;
-} | null
 
 export default async function StudentInfo({ params }: {params: {id: string}}) {
   try {
-    const student: Student = await getStudentById(params.id)
+    const student = await getStudentById(params.id)
     if (!student) {
       notFound()
     }
@@ -27,10 +12,10 @@ export default async function StudentInfo({ params }: {params: {id: string}}) {
     return (
       <section className="bg-slate-100 rounded-lg">
         <h1 className="text-2xl">
-          Name: {student?.firstName} {student?.lastName}
+          Name: {student.firstName} {student.lastName}
         </h1>
-        <p>Student ID Number: {student?.studentIdNumber}</p>
-        <p>School: {student?.school?.name}</p>
+        <p>Student ID Number: {student.studentIdNumber}</p>
+        <p>School: {student.school?.name}</p>
       </section>
     )
   } catch (error) {
