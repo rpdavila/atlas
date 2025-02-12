25555c8fa7bf5cbc9ed913edab154ef16dd50c06
rpdavila <rafael.pietri@gmail.com>
2025-02-12 16:45:08 +0100

    commented whole file for debugging purposes

--- src/lib/ReduxSSR/features/instrumentSLice.ts
+++ src/lib/ReduxSSR/features/instrumentSLice.ts
@@ -1,37 +1,37 @@
-//redux imports
-import {createSlice} from "@reduxjs/toolkit";
-//mongodb Imports
+// //redux imports
+// import {createSlice} from "@reduxjs/toolkit";
+// //mongodb Imports
 
-//type imports
-import {InstrumentList} from "@/app/types/formTypes";
+// //type imports
+// import {InstrumentList} from "@/app/types/formTypes";
 
 
-// initial state types
-type InstrumentState = {
-  instrumentList: InstrumentList;
-  instrumentSearch: string;
-  loading: boolean;
-  initialized: boolean;
-};
+// // initial state types
+// type InstrumentState = {
+//   instrumentList: InstrumentList;
+//   instrumentSearch: string;
+//   loading: boolean;
+//   initialized: boolean;
+// };
 
-const initialState: InstrumentState = {
-  instrumentList: [],
-  instrumentSearch: "",
-  loading: false,
-  initialized: false,
-};
+// const initialState: InstrumentState = {
+//   instrumentList: [],
+//   instrumentSearch: "",
+//   loading: false,
+//   initialized: false,
+// };
 
-export const instrumentDetailsSlice = createSlice({
-  name: "instrumentDetails",
-  initialState,
-  reducers: {
-    setInstrumentsInitialized: (state) => {
-      return {
-        ...state,
-        initialized: true
-      }     
-    }
-  },
-});
-export default instrumentDetailsSlice.reducer;
-export const {setInstrumentsInitialized} = instrumentDetailsSlice.actions;
+// export const instrumentDetailsSlice = createSlice({
+//   name: "instrumentDetails",
+//   initialState,
+//   reducers: {
+//   //   setInstrumentsInitialized: (state) => {
+//   //     return {
+//   //       ...state,
+//   //       initialized: true
+//   //     }     
+//   //   }
+//   // },
+// });
+// export default instrumentDetailsSlice.reducer;
+// export const {setInstrumentsInitialized} = instrumentDetailsSlice.actions;
