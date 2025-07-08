4372b4c9f9b17e2c2ed65f83a163f08a3c8bc789
rpdavila <rafael.pietri@gmail.com>
2025-07-08 10:58:18 +0200

    fixed auth and Dstrict components to show lading screen properly when useEffect is triggered

--- src/app/components/searchDistrictInstruments/searchDistrictInstruments.tsx
+++ src/app/components/searchDistrictInstruments/searchDistrictInstruments.tsx
@@ -1,6 +1,6 @@
 "use client";
-
-import { useMemo } from "react";
+// react imports
+import { useMemo, useEffect, useState } from "react";
 //types
 import { RootState } from "@/lib/ReduxSSR/store";
 //redux imports
@@ -8,6 +8,14 @@ import { useAppSelector } from "@/lib/ReduxSSR/hooks"
 import InstrumentSearchForm from "../forms/instrumentSearchForm";
 import DistrictInstrumentCardList from "../card-list/districtInstrumentCardList";
 import { RentStatus } from "@prisma/client";
+// action imports
+import { getInstrumentsByDistrict } from "@/actions/actions";
+// auth imports
+import { useSession } from "next-auth/react";
+// next imports
+import { redirect } from "next/navigation";
+import Loading from "../loading/loading";
+
 type DistrictInstrument = {
   school: {
     name: string;
@@ -22,16 +30,15 @@ type DistrictInstrument = {
 type DistrictInstruments = DistrictInstrument[]
 
 
-export default function SearchDistrictInstruments(
-  {
-    displayInstruments
-  }:
-    {
-      displayInstruments: DistrictInstruments
-    }) {
-
+export default function SearchDistrictInstruments() {
+  const session = useSession();
   const searchField = useAppSelector((state: RootState) => state.searchOptions.search)
-  
+  const [displayInstruments, setDisplayInstruments] = useState<DistrictInstruments | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  if (!session.data?.user?.id) {
+    redirect("/signIn");
+  }
   const instrumentSearchResults = useMemo(() => {
     if (!displayInstruments || !searchField.trim()) {
       return displayInstruments || [];
@@ -47,12 +54,37 @@ export default function SearchDistrictInstruments(
       )
     });
   }, [displayInstruments, searchField]);
+
+  useEffect(() => {
+    const getDistrictInstruments = async () => {
+      setLoading(true);
+      try {
+        if (!session.data.user?.id) return;
+        const districtData = await getInstrumentsByDistrict(session.data.user.id)
+        if (districtData?.length) {
+          setDisplayInstruments(districtData);
+        }
+        setLoading(false);
+
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching district instruments:", error);
+      }
+    }
+    getDistrictInstruments();
+
+  }, [session.data?.user?.id]);
   return (
     <section className={`flex flex-col basis-3/4 w-full items-center p-2 gap-2 h-svh`}>
       <section className="w-full md:hidden">
         <InstrumentSearchForm />
       </section>
-      <DistrictInstrumentCardList districtInstrumentSearchResults={instrumentSearchResults} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <DistrictInstrumentCardList districtInstrumentSearchResults={instrumentSearchResults} />
+      )}
+
     </section>
   )
 }
--- src/app/dashboard/districtInstruments/page.tsx
+++ src/app/dashboard/districtInstruments/page.tsx
@@ -7,24 +7,13 @@ import { auth } from "@/auth";
 //next imports
 import { permanentRedirect } from "next/navigation"
 // component imports
-import Loading from "@/app/components/loading/loading";
 import SearchDistrictInstruments from "@/app/components/searchDistrictInstruments/searchDistrictInstruments";
 
 export default async function DistrictInstruments() {
   const session = await auth()
   //redirect to signIn page if user is not signed in
   if (!session?.user) permanentRedirect("signIn")
-
-  const districtData = await getInstrumentsByDistrict(session?.user?.id as string)
-  if (districtData?.length) {
-    const displayInstruments = districtData;
-    return (
-      <Suspense fallback={<Loading />}>
-        <SearchDistrictInstruments displayInstruments={displayInstruments} />
-      </Suspense>
-    )
-  } else {
-    return <div className="text-center">No instruments found for your district</div>
-  }
-
+  return (
+    <SearchDistrictInstruments/>
+  )
 }
