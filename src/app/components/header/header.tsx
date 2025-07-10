20bd301a06f4934f57324e314e4120ce92ebd505
rpdavila <rafael.pietri@gmail.com>
2025-07-10 11:15:32 +0200

    fix: update signOut callback URL and refine Google provider import

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -125,7 +125,7 @@ export default function Header() {
             <NavbarItem>
               <Button
                 className="hover:bg-sky-700 hover:text-white" color="primary" variant="flat"
-                onPress={() => signOut({ callbackUrl: 'http://localhost:3000' })}
+                onPress={() => signOut({ callbackUrl: '/' })}
               >
                 Sign Out
               </Button>
@@ -137,7 +137,7 @@ export default function Header() {
             <NavbarItem className="hidden sm:flex">
               <Button
                 className="hover:bg-sky-700 hover:text-white" color="primary" variant="flat"
-                onPress={() => signIn()}
+                onPress={() => signIn('google', { callbackUrl: '/dashboard' })}
               >
                 Sign In
               </Button>
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
@@ -8,15 +8,7 @@ import prisma from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: PrismaAdapter(prisma),
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET
-		})
-	],
-	// pages: {
-	// 	signIn: "/signIn"
-	// },
+	providers: [Google],
 	callbacks: {
 		async session({ session, user }) {
 			if (session.user) {
