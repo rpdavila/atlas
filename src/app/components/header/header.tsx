631175c302d85125f62e9fe6ccc49bccddd5e49e
Rafael Pietri-Davila <rafael.pietri@gmail.com>
2025-10-25 16:08:09 +0200

    ADDED LINKEDIN Social Sign in

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -138,7 +138,7 @@ export default function Header() {
               <Button
                 data-cy="sign-in"
                 className="hover:bg-sky-700 hover:text-white" color="primary" variant="flat"
-                onPress={() => signIn('google', { redirectTo: '/dashboard' })}
+                onPress={() => signIn()}
               >
                 Sign In
               </Button>
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,13 @@
 
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import LinkedIn from "next-auth/providers/linkedin";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: PrismaAdapter(prisma),
-	providers: [Google],
+	providers: [Google, LinkedIn],
 	callbacks: {
 		async session({ session, user }) {
 			if (session.user) {
