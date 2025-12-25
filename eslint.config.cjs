0ba80f2173b4e132aad79289069e7e1ac7b9591e
Rafael Pietri-Davila <rafael.pietri@gmail.com>
2025-12-25 23:24:43 +0100

    updated eslint json to eslint config file and added module to package.json

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
+export default [
+js.configs.recommended,
+
+{
+files: ["**/*.{js,jsx,ts,tsx}"],
+plugins: {
+react,
+},
+rules: {
+"react/react-in-jsx-scope": "off"
+},
+settings: {
+react: {
+version: "detect"
+}
+}
+}
+];
+
